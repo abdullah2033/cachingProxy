8731b6081544ad941e7774bd605d6fa03fc09634
abdullah <you@example.com>
2025-04-21 16:46:44 +0500

    test

---
+++ index.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+import  http from 'http';
+http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello World');
+}).listen(8081);
+
+
